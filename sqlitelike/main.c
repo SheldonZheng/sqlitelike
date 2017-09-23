8102c6e14bf29eefb0022809101a6aa4f47e3807
Baiye <zhenghangtxdyr@gmail.com>
2017-09-23 20:56:54 +0800

    REPL;

--- sqlitelike/main.c
+++ sqlitelike/main.c
@@ -1,17 +1,19 @@
-#include<stdio.h>
-#include<unistd.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
-struct InputBuffer_t 
-{
+
+
+struct InputBuffer_t {
 	char* buffer;
 	size_t buffer_length;
-	ssize_t input_length;
+	size_t input_length;
 };
-
 typedef struct InputBuffer_t InputBuffer;
 
-InputBuffer* new_input_buffer() 
-{
+InputBuffer* new_input_buffer() {
 	InputBuffer* input_buffer = malloc(sizeof(InputBuffer));
 	input_buffer->buffer = NULL;
 	input_buffer->buffer_length = 0;
@@ -20,32 +22,85 @@ InputBuffer* new_input_buffer()
 	return input_buffer;
 }
 
-void print_prompt() 
-{
-	printf("db > ");
+size_t getline(char **lineptr, size_t *n, FILE *stream) {
+	char *bufptr = NULL;
+	char *p = bufptr;
+	size_t size;
+	int c;
+
+	if (lineptr == NULL) {
+		return -1;
+	}
+	if (stream == NULL) {
+		return -1;
+	}
+	if (n == NULL) {
+		return -1;
+	}
+	bufptr = *lineptr;
+	size = *n;
+
+	c = fgetc(stream);
+	if (c == EOF) {
+		return -1;
+	}
+	if (bufptr == NULL) {
+		bufptr = malloc(128);
+		if (bufptr == NULL) {
+			return -1;
+		}
+		size = 128;
+	}
+	p = bufptr;
+	while (c != EOF) {
+		if ((p - bufptr) > (size - 1)) {
+			size = size + 128;
+			bufptr = realloc(bufptr, size);
+			if (bufptr == NULL) {
+				return -1;
+			}
+		}
+		*p++ = c;
+		if (c == '\n') {
+			break;
+		}
+		c = fgetc(stream);
+	}
+
+	*p++ = '\0';
+	*lineptr = bufptr;
+	*n = size;
+
+	return p - bufptr - 1;
 }
 
-ssize_t getline(char **lineptr, size_t  *n, FILE *stream);
+void print_prompt() { printf("db > "); }
 
+void read_input(InputBuffer* input_buffer) {
+	size_t bytes_read =
+		getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
 
-int main(int argc, char* argv[]) 
-{
+	if (bytes_read <= 0) {
+		printf("Error reading input\n");
+		exit(EXIT_FAILURE);
+	}
+
+	// Ignore trailing newline
+	input_buffer->input_length = bytes_read - 1;
+	input_buffer->buffer[bytes_read - 1] = 0;
+}
+
+int main(int argc, char* argv[]) {
 	InputBuffer* input_buffer = new_input_buffer();
-	while (1) 
-	{
+	while (true) {
 		print_prompt();
 		read_input(input_buffer);
 
-		if (strcmp(input_buffer->buffer, "exit") == 0) 
-		{
+		if (strcmp(input_buffer->buffer, ".exit") == 0) {
 			exit(EXIT_SUCCESS);
 		}
-		else 
-		{
+		else {
 			printf("Unrecognized command '%s'.\n", input_buffer->buffer);
 		}
 	}
-	printf("Hello world");
-	getchar();
-	return 0;
-} 
+}
